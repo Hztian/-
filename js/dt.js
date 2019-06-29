18dc812b58b91a220a9821085de5fbaf1f3505b2
黄镇添 <hztian@github.com>
2019-06-29 11:30:48 +0800

    修改

---
+++ js/dt.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	$(".banner").fadeIn("slow");
+	$("main").fadeIn(2000);
+});
+window.onscroll = function(e) {
+	var e = e || window.event;
+	var scrolltop = document.documentElement.scrollTop || document.body.scrollTop;
+
+	if (scrolltop > 0 && scrolltop < 139) {
+		$(".skills").fadeIn(2000);
+	}
+	if (scrolltop > 400) {
+		$(".portfolio").fadeIn(2000);
+	}
+}
